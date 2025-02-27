6882964d5200e45915424cd5d53447462324458a
juanlam7 <juanlam7@gmail.com>
2025-02-27 22:49:36 +0100

    feat: add tailwindcss and the first page

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent {}
