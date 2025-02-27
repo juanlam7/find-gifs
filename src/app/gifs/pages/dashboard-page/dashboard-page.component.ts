3a370e984142de24141b8774b604f31573ac64a7
juanlam7 <juanlam7@gmail.com>
2025-02-27 23:09:18 +0100

    feat: Add other pages with their routes

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
+  imports: [RouterOutlet],
   templateUrl: './dashboard-page.component.html',
 })
 export default class DashboardPageComponent {}
---
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-history',
+  templateUrl: './gif-history.component.html',
+})
+export default class GifHistoryComponent {}
