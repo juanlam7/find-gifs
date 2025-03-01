175d1d5d4f93e438dcb4c398a72116ff3e90f744
juanlam7 <juanlam7@gmail.com>
2025-03-01 18:49:18 +0100

    feat: Add @ngx-env/builder for .env file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: true,
-    companyName: 'Gifs',
-    companyName2: 'App',
-    companySlogan: 'Maneja tus gifs',
-    giphyApiKey: 'giphyApiKey',
-    giphyUrl: 'giphyUrl',
-  };
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  giphyApiKey: import.meta.env.NG_APP_GIPHY_API_KEY,
+  giphyUrl: import.meta.env.NG_APP_GIPHY_URL,
+};
